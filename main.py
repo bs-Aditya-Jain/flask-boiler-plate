1269f023374dcc0d96bf79608e456d4752a69bc2
Aditya Jain <aditya.jain@bombaysoftwares.com>
2025-02-03 11:56:01 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Importing libraries
+"""
+This is the main file for the application.
+It creates the flask app and runs it.
+"""
+from app import create_app
+application = create_app()
+
+if __name__ == '__main__':
+    application.run(application)  # type: ignore  # noqa: attribute-error
